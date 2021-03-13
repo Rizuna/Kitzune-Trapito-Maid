6b7a8df55266e553d266badd990ea75d87a37b80
Rizuna <78313461+Rizuna@users.noreply.github.com>
2021-03-13 16:21:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ const vcard = 'BEGIN:VCARD\n'
 + 'VERSION:3.0\n'
 + 'FN:Mrf.zvx\n' // Nama
 + 'ORG:Lexa bot;\n' // Nama bot
-+ 'TEL;type=CELL;type=VOICE;waid=6282223014661:+56 9 4668 2809\n' // Nomor bot
++ 'TEL;type=CELL;type=VOICE;waid=6282223014661:+62 822-2301-4661\n' // Nomor bot
 + 'END:VCARD' 
 
 
@@ -3797,7 +3797,7 @@ break
 
 				default:
 				if (body.startsWith(`${prefix}${command}`)) {
-  reply(`        ────────────────\nHola *${pushname}* !!!\nMando/comando : *${prefix}${command}*\nIncorrecto *${prefix}Menu*\n        ────────────────`)
+  reply(`        ────────────────\nHei *${pushname}* !!!\nPerintah/Command : *${prefix}${command}*\nTidak ada dalam *${prefix}Menu*\n        ────────────────`)
 				}
 					if (isGroup && isSimi && budy != undefined) {
 						console.log(budy)
